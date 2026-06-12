9b64ec7a0a5ebd6b600df4eded140c44820c750b
Scott A. Idler <scott.a.idler@gmail.com>
2026-06-11 21:17:55 -0700

    chore: clear clippy 1.96.0 -D warnings debt

--- src/history/mod.rs
+++ src/history/mod.rs
@@ -344,7 +344,7 @@ impl HistoryStore {
         }
 
         // Sort by date (newest first)
-        entries.sort_by(|a, b| b.created_at.cmp(&a.created_at));
+        entries.sort_by_key(|b| std::cmp::Reverse(b.created_at));
         Ok(entries)
     }
 }
